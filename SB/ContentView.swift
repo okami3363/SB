5357b3f69c64f471eb56a3de1bc4c74628937a71
okami3363 <okami3363@gmail.com>
2025-11-08 02:48:18 +0800

    Initial Commit

---
+++ SB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SB
+//
+//  Created by 黃崑展 on 2025/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SB/SBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBApp.swift
+//  SB
+//
+//  Created by 黃崑展 on 2025/11/8.
+//
+
+import SwiftUI
+
+@main
+struct SBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
